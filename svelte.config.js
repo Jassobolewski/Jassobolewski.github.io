1a5bcf1cbd5db1529c3c6a3f964ddf547c1475e9
Jan Sobolewski <59810745+Jassobolewski@users.noreply.github.com>
2025-05-03 02:34:44 -0400

    env

--- svelte.config.js
+++ svelte.config.js
@@ -13,8 +13,7 @@ const config = {
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
     adapter: adapter(),
     paths: {
-      base:
-        process.env.Node_ENV === "production" ? "/jassobolewski.github.io" : "",
+      base: dev ? "" : process.env.BASE_PATH,
     },
   },
 };
