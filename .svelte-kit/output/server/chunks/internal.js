16f3df47f385b81c25e0fe17479e7db4b341732a
Jan Sobolewski <59810745+Jassobolewski@users.noreply.github.com>
2025-05-03 03:26:04 -0400

    more fixes

--- .svelte-kit/output/server/chunks/internal.js
+++ .svelte-kit/output/server/chunks/internal.js
@@ -548,7 +548,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "1e7s9g"
+  version_hash: "1i0pblq"
 };
 async function get_hooks() {
   let handle;
--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,10 @@
 import adapter from "@sveltejs/adapter-static";
-const dev = process.env.NODE_ENV === "development";
-const repoName = "Jassobolewski.github.io"; // your repo name
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter({
-      fallback: "404.html",
+      fallback: "index.html",
     }),
     paths: {
       base: "",
